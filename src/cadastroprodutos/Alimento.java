c156f78ddbfa2a342da41ae806c37c8ab70a0de0
Vinícius Zanella <34425082+viniciuszanella@users.noreply.github.com>
2025-03-11 09:43:27 -0300

    Update Alimento.java

--- src/cadastroprodutos/Alimento.java
+++ src/cadastroprodutos/Alimento.java
@@ -8,7 +8,7 @@ public class Alimento extends Produto{
     private LocalDate data_validade;
     private String categoria;
 
-    // Construtor da classe Alimento
+    // Construtor da classe
     public Alimento(String codigo, String nome, double preco, LocalDate data_validade, String categoria) {
         super(codigo, nome, preco);
         this.data_validade = data_validade;
